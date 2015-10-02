14166a64e83ca58ea9b2780d6f903095e65a6253
AleayaDixon <dixonaleaya@gmail.com>
2015-10-02 03:08:08 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+// this will be the javascript file to hold all of the code for my first app
+// create the angular module
+//this places the code in a sort of container so that name collision can be avoided
+(function () {
+    'use strict';
+    var myApp = angular.module('app', ['ionic', 'App.Controllers'])
+
+    myApp.config(function($stateProvider, $urlRouterProvider) {
+        //
+        // For any unmatched url, redirect to /home
+        $urlRouterProvider.otherwise("/home");
+        //
+        // Now set up the states
+        $stateProvider
+            .state('home', {
+                url: "/home",
+                templateUrl: "views/home.html",
+                controller :"homeCtrl"
+            })
+            .state('detail', {
+                url: "/detail/:objectId",
+                templateUrl: "views/detail.html",
+                controller :"detailCtrl"
+            });
+    });
+})();
